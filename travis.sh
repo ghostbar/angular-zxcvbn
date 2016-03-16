8b7e69a763dc31bba971740cac2a60c4ecdc0e11
Jose-Luis Rivas <ghostbar@users.noreply.github.com>
2016-03-15 19:21:23 -0500

    typo: missing `coverage`

--- travis.sh
+++ travis.sh
@@ -9,7 +9,7 @@ if [ $TEST = "unit-tests" ]; then
   sh -e /etc/init.d/xvfb start
   sleep 1
   gulp test
-  CODECLIMATE_REPO_TOKEN=6ba4238a4867ac84b253a1f39cedc9ea9f04197e26d4597c92ed1f87ab0331eb codeclimate-test-reporter < lcov.info
+  CODECLIMATE_REPO_TOKEN=6ba4238a4867ac84b253a1f39cedc9ea9f04197e26d4597c92ed1f87ab0331eb codeclimate-test-reporter < coverage/lcov.info
 
 elif [[ $TEST = "browser-tests" ]]; then
 
